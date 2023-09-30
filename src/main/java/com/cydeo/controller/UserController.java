77c9e3a55ce054e9dbcfa683cb9d3bef8e0e642c
feruzka1706@gmail.com <feruzka1706@gmail.com>
2023-09-30 09:11:02 -0500

    Part-2 code changes are completed

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -2,12 +2,14 @@ package com.cydeo.controller;
 
 
 import com.cydeo.dto.UserDTO;
+import com.cydeo.service.RoleService;
 import com.cydeo.service.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,16 +17,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @AllArgsConstructor
 public class UserController {
 
-   UserService userService;
+    UserService userService;
+    RoleService roleService;
 
     @GetMapping("/create")
     public String userCreateView(Model model){
 
         model.addAttribute("user", new UserDTO());
-        model.addAttribute("roles",userService.findAll());
+        //bring all roles from DB
+        model.addAttribute("roles",roleService.findAll());
+        model.addAttribute("users",userService.findAll());
 
         return "user/create";
     }
 
+    //After Spring 4.3 we don't have to explicitly pass @ModelAttribute("user")
+    //Spring configures implicitly
+
+    @PostMapping("/create")
+    public String userDisplayView( @ModelAttribute("user") UserDTO userDTO){
+
+        userService.save(userDTO);
+
+        //return "user/create"; //user, roles, users
+        return "redirect:/user/create"; //redirecting above controller method
+
+    }
 
 }
---
+++ src/main/java/com/cydeo/service/impl/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.UserDTO;
+import com.cydeo.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl extends AbstractMapService<UserDTO,String> implements UserService {
+
+
+    @Override
+    public UserDTO save(UserDTO object) {
+        return super.save(object.getEmail(),object);
+    }
+
+    @Override
+    public List<UserDTO> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public UserDTO findById(String id) {
+        return super.findBydId(id);
+    }
+
+
+    @Override
+    public void deleteById(String id) {
+        super.deleteById(id);
+    }
+}
